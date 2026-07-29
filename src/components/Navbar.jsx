b779b9443c1f4fe512e67c47fbe8bf0b51f16522
Akam <adam@MacBook-Pro-som-tillhor-Adam.local>
2026-07-29 04:01:44 +0200

    Add Gallery link back to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import './Navbar.css'
 const navLinks = [
   { path: '/', label: 'Hem' },
   { path: '/about', label: 'Om Oss' },
+  { path: '/gallery', label: 'Galleri' },
   { path: '/news', label: 'Nyheter' },
   { path: '/meetups', label: 'Meetups' },
   { path: '/merch', label: 'Merch' },
